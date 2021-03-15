f7fb1c69b507dd2dc11645942470c30017497a67
oliverlsanz <oliverlsanz@gmail.com>
2021-03-15 14:15:24 +0100

    work on toggle goal, we still have to toggle the right one

--- src/redux/GoalsSlice.js
+++ src/redux/GoalsSlice.js
@@ -4,18 +4,25 @@ const goalsAdapter = createEntityAdapter();
 
 const initialState = goalsAdapter.getInitialState({nextId: 0});
 
+// each goal is {name: str, motivation: str, activated: bool}
+
 const goalsSlice = createSlice({
   name: 'goals',
   initialState,
   reducers: {
     createGoal(state, action){
-      goalsAdapter.addOne(state, {...action.payload, id: state.nextId.toString()})
+      goalsAdapter.addOne(state, {...action.payload, id: state.nextId.toString(), activated: true})
       state.nextId += 1
     },
+    toggleGoal(state, action){
+      const goalId = action.payload.id
+      const newValue = !state.entities[goalId].activated
+      goalsAdapter.updateOne(state, {id: goalId, changes: {activated: newValue}})
+    }
   }
 })
 
-export const { createGoal } = goalsSlice.actions
+export const { createGoal, toggleGoal } = goalsSlice.actions
 
 export const { selectAll: selectAllGoals } = goalsAdapter.getSelectors(state => state.goals)
 
--- src/screens/GoalsScreen/GoalsScreen.js
+++ src/screens/GoalsScreen/GoalsScreen.js
@@ -2,30 +2,38 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { FlatList, View } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
-import { List, Switch, Appbar } from 'react-native-paper';
+import { List, Switch, Appbar, Text } from 'react-native-paper';
 import { Header } from '../../components'
-import { selectAllGoals } from '../../redux/GoalsSlice'
+import { selectAllGoals, toggleGoal } from '../../redux/GoalsSlice'
 
 
-const GoalListItem = ({ name, activated }) => {
+const GoalListItem = ({ name, activated, toggleGoal }) => {
   const navigation = useNavigation();
 
   return (
-    <List.Item
-      title={name}
-      right={() => <Switch value={activated}/>}
-      onPress={() => navigation.navigate('Goal', { headerTitle: name, goalName: name })}
+    <List.Item 
+      title={
+        <View style={{width: '100%'}}>
+          <Text 
+            onPress={() => navigation.navigate('Goal', { headerTitle: name, goalName: name })}>
+              {name}
+          </Text>
+        </View>}
+       right={() => <Switch value={activated} onValueChange={ () => toggleGoal({id: 0}) }/>}
     />
   );
 }
 
-const renderItem = ({ item }) => (
-  <GoalListItem
-    name={item.name}
-    activated={item.activated} />
-)
 
-const GoalsScreen = ({ navigation, goals }) => {
+
+const GoalsScreen = ({ navigation, goals, toggleGoal }) => {
+  const renderItem = ({ item }) => (
+    <GoalListItem
+      name={item.name}
+      activated={item.activated} 
+      toggleGoal={toggleGoal}/>
+  )
+
   return(
     <View>
       <Header 
@@ -43,4 +51,8 @@ const mapStateToProps = (state) => {
   return { goals }
 };
 
-export default connect(mapStateToProps)(GoalsScreen);
+const actionsToProps = {
+  toggleGoal,
+}
+
+export default connect(mapStateToProps, actionsToProps)(GoalsScreen);
