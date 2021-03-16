4290e12268d906e85df13de8776b3bdffe045189
oliverlsanz <oliverlsanz@gmail.com>
2021-03-16 11:22:25 +0100

    add ActivitySlice

---
+++ src/redux/ActivitySlice.js
@@ -0,0 +1,42 @@
+import { createSlice, createEntityAdapter } from '@reduxjs/toolkit'
+
+const activityAdapter = createEntityAdapter();
+
+const initialState = activityAdapter.getInitialState({nextId: 0});
+
+/*
+each activity is:
+{ 
+  goalId: id of its goal
+  name: str
+  active: bool
+  repeatMode: "daily" / "weekly" / "weekdays",
+  goal: "check" / "time"
+  timeGoal: seconds, 
+  weekdays: {'monday': bool, 'tuesday': bool, 'wednesday': bool, 'thursday': bool', 
+    'friday': bool, 'saturday': bool, 'sunday': bool'}, 
+  timesPerWeek: int 
+}
+*/
+
+const activitySlice = createSlice({
+  name: 'activities',
+  initialState,
+  reducers: {
+    createActivity(state, action){
+        activityAdapter.addOne(state, {...action.payload, id: state.nextId.toString(), active: true})
+        state.nextId += 1
+    },
+    toggleActivity(state, action){
+        const activityId = action.payload.id
+        const newValue = !state.entities[activityId].active
+        activityAdapter.updateOne(state, {id: activityId, changes: {active: newValue}})
+    },
+  }
+})
+
+export const { createActivity, toggleActivity } = activitySlice.actions
+
+export const { selectAll: selectAllActivities } = activityAdapter.getSelectors(state => state.activities)
+
+export default activitySlice.reducer
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import goalsReducer from './GoalsSlice'
+import activitySlice from './ActivitySlice'
 
 const store = configureStore({
   reducer: {
     goals: goalsReducer,
+    activities: activitySlice,
   }
 })
 
