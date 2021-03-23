cd9659235b8255c9f4ebe35c9099c99012837057
oliverlsanz <oliverlsanz@gmail.com>
2021-03-23 09:46:23 +0100

    make redux state persistent

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 import { DateTime } from 'luxon'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 import { 
   TodayScreen, WeekScreen, ActivityDetailScreen, GoalsScreen, GoalScreen, ActivityFormScreen,
   GoalFormScreen
@@ -14,6 +16,8 @@ import { store, generateDummyData, updateLogs } from './src/redux'
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
+
+const persistor = persistStore(store)
 
 const TodayStack = () => (
   <Stack.Navigator initialRouteName='Today' headerMode='none'>
@@ -43,21 +47,23 @@ const GoalsStack = () => (
 
 export default function App() {
   useEffect(() => {
-    store.dispatch(generateDummyData())
+    // store.dispatch(generateDummyData())
     store.dispatch(updateLogs())
   })
 
   return (
     <ReduxProvider store={store}>
-      <PaperProvider>
-        <NavigationContainer>
-          <Drawer.Navigator initialRouteName='Today'>
-            <Drawer.Screen name='Today' component={TodayStack} />
-            <Drawer.Screen name='Week' component={WeekStack} />
-            <Drawer.Screen name='Goals' component={GoalsStack} />
-          </Drawer.Navigator>
-        </NavigationContainer>
-      </PaperProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <PaperProvider>
+          <NavigationContainer>
+            <Drawer.Navigator initialRouteName='Today'>
+              <Drawer.Screen name='Today' component={TodayStack} />
+              <Drawer.Screen name='Week' component={WeekStack} />
+              <Drawer.Screen name='Goals' component={GoalsStack} />
+            </Drawer.Navigator>
+          </NavigationContainer>
+        </PaperProvider>
+      </PersistGate>
     </ReduxProvider>
   );
 }
