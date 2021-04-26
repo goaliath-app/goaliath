goaliath-app/goaliath

commit 766bbfb6c88446fcb57829b4d812228273620b10

improve ActivityDetailScreen UI
minor ui improvements:
* use of the new TimeInput
* add keyboard avoiding
* add label to checkbox

Also:
* The new TimeInput has been refactored to make it reusable.
* The old TimeInput has been deleted (replaced by the new one).

src/screens/ActivityDetailScreen/TodayPannel.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { View } from 'react-native'
-import { Button, List, Checkbox, Divider } from 'react-native-paper';
+import { Button, List, Checkbox, Divider, Paragraph } from 'react-native-paper';
 import { TimeInput } from '../../components';
 import { getTodayTime, isActivityRunning, isToday, startOfDay } from '../../util'
-import { DateTime } from 'luxon';
+import { DateTime, Duration } from 'luxon';
 import { useTranslation } from 'react-i18next'
 
 const TodayPannel = ({ entry, toggleCompleted, startTimer, stopTimer, upsertEntry, date, dayStartHour }) => {
@@ -62,6 +62,7 @@ const TodayPannel = ({ entry, toggleCompleted, startTimer, stopTimer, upsertEntr
     }
   
     const dateIsToday = isToday(date, dayStartHour)
+    const activityRunning = isActivityRunning(entry.intervals)
 
     let seconds, minutes, hours
     seconds = String(todayTime.seconds).padStart(2, '0')
@@ -72,16 +73,37 @@ const TodayPannel = ({ entry, toggleCompleted, startTimer, stopTimer, upsertEntr
       <View>
         <List.Item
           title={t('todayPannel.title')}
-          right={() => <Checkbox status={entry.completed? 'checked':'unchecked'} onPress={() => {toggleCompleted({date: date, id: entry.id})} }/>}
+          right={() => (
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Paragraph style={{marginRight: 6}}>{t('todayPannel.checkboxLabel')}</Paragraph>
+              <Checkbox 
+                status={entry.completed? 'checked':'unchecked'} 
+                onPress={() => {toggleCompleted({date: date, id: entry.id})} }
+              />
+            </View>
+          )}
         />
-        <TimeInput seconds={seconds} minutes={minutes} hours={hours} setHours={setHours} setMinutes={setMinutes} setSeconds={setSeconds} />
-        {dateIsToday?
-          (isActivityRunning(entry.intervals)?
-            <Button onPress={onPressPause}>{t('todayPannel.stopButton')}</Button>
-          :
-            <Button onPress={onPressPlay}>{t('todayPannel.startButton')}</Button>)
-        : null
-        }
+          <TimeInput 
+            regularColor={activityRunning?'#6200C5':'black'}
+            value={todayTime.as('seconds')} 
+            onValueChange={(value) => { 
+              setTodayTime(
+                Duration
+                  .fromObject({ seconds: value })
+                  .shiftTo('hours', 'minutes', 'seconds')
+              ) 
+            }} 
+            />
+          {dateIsToday?
+            (activityRunning?
+              <Button onPress={onPressPause}>{t('todayPannel.stopButton')}</Button>
+              :
+              <Button 
+                onPress={onPressPlay}
+                >
+                  {t('todayPannel.startButton')}</Button>)
+              : null
+            }
         <Divider />
       </View>
     )

src/Translations/english.js
@@ -51,8 +51,8 @@ const en = {
     todayPannel: {
       title: 'Today',
       stopButton: 'Stop Timer',
-      startButton: 'Start Timer'
-
+      startButton: 'Start Timer',
+      checkboxLabel: 'Done',
     },
 
     activityForm: {

src/Translations/spanish.js
@@ -51,8 +51,8 @@ const es= {
     todayPannel: {
       title: 'Hoy',
       stopButton: 'Parar temporizador',
-      startButton: 'Empezar temporizador'
-
+      startButton: 'Empezar temporizador',
+      checkboxLabel: 'Hecho',
     },
 
     activityForm: {

src/components/TimeInput.js
@@ -1,38 +1,124 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Text, TextInput } from 'react-native-paper';
+import { Duration } from 'luxon'
 
-const TimeInput = ({ hours, setHours, minutes, setMinutes, seconds, setSeconds }) => (
-    <View style={{flexDirection: 'row', marginLeft: 20, marginRight: 20}}>
-  
-      <TextInput 
-        style={{flex: 2, fontSize: 40, padding: 5, textAlign: 'center'}} keyboardType='numeric'
-        value={hours} 
-        onChangeText={setHours}
-        selectTextOnFocus={true}
-        maxLength={2}
-      />
-      <Text style={{flex: 1, fontSize: 50,  textAlign: 'center'}}>:</Text>
-  
-      <TextInput 
-        style={{flex: 2, fontSize: 40, padding: 5, textAlign: 'center'}} keyboardType='numeric'
-        value={minutes} 
-        onChangeText={setMinutes}
-        selectTextOnFocus={true}
-        maxLength={2}
-      />
-      <Text style={{flex: 1, fontSize: 50, textAlign: 'center'}}>:</Text>
-  
-      <TextInput 
-        style={{flex: 2, fontSize: 40, padding: 5, textAlign: 'center'}} keyboardType='numeric' 
-        value={seconds} 
-        onChangeText={setSeconds}
-        selectTextOnFocus={true}
-        maxLength={2}
-      />
-     
-  
-    </View>
+const TimeInput = ({ value, onValueChange, regularColor='black', selectedColor='#7B61FF' }) => {
+
+  const [currentFocus, setCurrentFocus] = React.useState()
+  const [selection, setSelection] = React.useState()
+
+  const duration = (
+    Duration
+      .fromObject({ seconds: value })
+      .shiftTo('hours', 'minutes', 'seconds')
   )
 
-  export default TimeInput;
+  const hours   = duration.hours
+  const minutes = duration.minutes
+  const seconds = duration.seconds
+
+  const getTheme = isFocused =>(
+    { colors: { primary: 'transparent', text: isFocused?selectedColor:regularColor } }
+  )
+  
+  const commonProps = ({ unit, maxValue }) => ({
+    style: {
+      fontSize: 50, 
+      padding: 5, 
+      textAlign: 'center', 
+      backgroundColor: 'transparent', 
+      underlineColorAndroid: 'transparent',
+    },
+    selection: selection,
+    autoCorrect: false,
+    selectOnFocus: true,
+    maxLength: 2,
+    keyboardType: 'number-pad',
+    caretHidden: true,
+    underlineColor: 'transparent',
+    selectionColor: 'transparent',
+    theme: getTheme(currentFocus==unit),
+    onBlur: () => { 
+      if(currentFocus==unit) setCurrentFocus('')
+      setSelection(null)
+    },
+    onChangeText: (value) => {
+      console.log(value)
+      value = value?parseInt(value):0
+
+      if(String(value).length < 2){
+        setSelection({start: value.length, end: value.length})
+      }else{
+        setSelection({start: 0, end: 2})
+      }
+      
+      value = value<maxValue?value:maxValue
+      const newDuration = duration.set({ [unit]: value })
+      console.log(newDuration.as('seconds'))
+      onValueChange(newDuration.as('seconds'))
+    },
+  })
+  
+  return(
+    <View 
+      style={{
+        flexDirection: 'row', 
+        marginLeft: 20, 
+        marginRight: 20, 
+        justifyContent: 'center', 
+        alignItems: 'center'
+      }
+    }>
+
+      <TextInput 
+        value={
+          currentFocus=='hours'?
+            hours.toString()
+          :
+            hours.toString().padStart(2, '0')
+        } 
+        onFocus= {() => {
+          setSelection({start:0, end: hours.toString().length})
+          setCurrentFocus('hours')
+        }}
+        {...commonProps({ unit: 'hours', maxValue: 23 })} 
+      />
+
+      <Text style={{fontSize: 50, marginBottom: 5, color: regularColor}}>:</Text>
+      
+      <TextInput 
+        value={
+          currentFocus=='minutes'?
+            minutes.toString()
+          :
+            minutes.toString().padStart(2, '0')
+        } 
+        onFocus= {() => {
+          setSelection({start:0, end: minutes.toString().length})
+          setCurrentFocus('minutes')
+        }}
+        {...commonProps({ unit: 'minutes', maxValue: 59 })} 
+      />
+
+      <Text style={{fontSize: 50, marginBottom: 5, color: regularColor}}>:</Text>
+
+      <TextInput 
+        value={
+          currentFocus=='seconds'?
+            seconds.toString()
+          :
+            seconds.toString().padStart(2, '0')
+        } 
+        onFocus= {() => {
+          setSelection({start:0, end: seconds.toString().length})
+          setCurrentFocus('seconds')
+        }}
+        {...commonProps({ unit: 'seconds', maxValue: 59 })}  
+      />
+
+    </View>
+  )
+}
+
+export default TimeInput

src/screens/ActivityDetailScreen/ActivityDetailScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native'
+import { View, KeyboardAvoidingView, ScrollView } from 'react-native'
 import { connect } from 'react-redux';
 import { Paragraph, Menu, Subheading, Title, Divider, List } from 'react-native-paper';
 import { DateTime } from 'luxon'
@@ -66,34 +66,33 @@ const ActivityDetailScreen = ({
 
 
   return(
-    <View style={{ backgroundColor: 'white', flex: 1 }}>
-      <View>
-        <Header title={activity.name} left='back' navigation={navigation} buttons={headerButtons} />
+    <KeyboardAvoidingView style={{flex:1}}>
+      <Header title={activity.name} left='back' navigation={navigation} buttons={headerButtons} />
+      <ScrollView style={{ backgroundColor: 'white', flex: 1 }}>
         {date && !dateIsToday? 
         <>
-          <List.Item 
-            title={<Title>{dateTitle}</Title>} 
-            right={() => (
-              <View style={{alignSelf: 'center', paddingRight: 12}}>
-                <HelpIcon dialogContent={
-                  <Paragraph>{t('activityDetail.helpIconText')}</Paragraph>
-                }/>
-              </View>
-            )}
-          />
-          <Divider />
+        <List.Item 
+          title={<Title>{dateTitle}</Title>} 
+          right={() => (
+            <View style={{alignSelf: 'center', paddingRight: 12}}>
+              <HelpIcon dialogContent={
+                <Paragraph>{t('activityDetail.helpIconText')}</Paragraph>
+              }/>
+            </View>
+          )}
+        />
+        <Divider />
         </>
         : null
         }
         <BasicActivityInfo activity={activity} goal={goal} />
-  
         {dateIsToday?
-          <TodayPannel entry={entry} toggleCompleted={toggleCompleted} startTimer={startTimer} stopTimer={stopTimer} upsertEntry={upsertEntry} date={date} dayStartHour={dayStartHour} /> 
-          : 
+        <TodayPannel entry={entry} toggleCompleted={toggleCompleted} startTimer={startTimer} stopTimer={stopTimer} upsertEntry={upsertEntry} date={date} dayStartHour={dayStartHour} /> 
+        : 
           entry?
-            <TodayPannel entry={entry} toggleCompleted={toggleCompleted} startTimer={()=>{}} stopTimer={()=>{}} upsertEntry={upsertEntry} date={date} dayStartHour={dayStartHour} />
-            :
-            null
+          <TodayPannel entry={entry} toggleCompleted={toggleCompleted} startTimer={()=>{}} stopTimer={()=>{}} upsertEntry={upsertEntry} date={date} dayStartHour={dayStartHour} />
+          :
+          null
         }
 
         {/* future work:
@@ -101,7 +100,7 @@ const ActivityDetailScreen = ({
         <GenericStats /> 
         */}
 
-      </View>
+      </ScrollView>
 
       <DeleteDialog 
         visible={deleteDialogVisible} 
@@ -114,8 +113,7 @@ const ActivityDetailScreen = ({
         title={t('activityDetail.deleteDialog.title')}
         body={t('activityDetail.deleteDialog.body')}
       />
-
-    </View>
+    </KeyboardAvoidingView>
   )
 }
 

src/screens/ActivityFormScreen/ActivityFormScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { View, StyleSheet, Alert, Pressable, FlatList, KeyboardAvoidingView, Platform, ScrollView, Keyboard } from 'react-native';
 import { Subheading, Appbar, Text, TextInput, Button, List, Checkbox, Title, Paragraph, HelperText, Caption } from 'react-native-paper';
-import { Header, HelpIcon } from '../../components';
+import { Header, HelpIcon, TimeInput } from '../../components';
 import { createActivity, updateActivity, selectActivityById } from '../../redux'
 import { useTranslation } from 'react-i18next'
 
@@ -18,9 +18,9 @@ const ActivityFormScreen = ({ navigation, createActivity, updateActivity, goalId
   const [name, setName] = React.useState(activity?.name?activity.name:'');
   const [repeatMode, setRepeatMode] = React.useState(activity?.repeatMode?activity.repeatMode:'daily');  // 'daily', 'select' or 'weekly'
   const [goal, setGoal] = React.useState(activity?.goal?activity.goal:'check');  // 'time' or 'check'
-  const [seconds, setSeconds] = React.useState(activity?.timeGoal?(activity.timeGoal%60).toString().padStart(2, '0'):'00');
-  const [minutes, setMinutes] = React.useState(activity?.timeGoal?(Math.floor((activity.timeGoal%3600)/60)).toString().padStart(2, '0'):'00');
-  const [hours, setHours] = React.useState(activity?.timeGoal?(Math.floor((activity.timeGoal/3600))).toString().padStart(2, '0'):'00');
+
+  const [timeGoal, setTimeGoal] = React.useState(activity?.timeGoal?activity.timeGoal:0);
+
   const [weekDays, setWeekDays] = React.useState(activity?.weekDays?activity.weekDays:{
     '1': false, '2': false, '3': false, '4': false, '5': false, '6': false, '7': false
   })
@@ -64,17 +64,6 @@ const ActivityFormScreen = ({ navigation, createActivity, updateActivity, goalId
   const headerButtons = (
     <Appbar.Action icon='check' onPress={() => {
         Keyboard.dismiss()
-        let timeGoal
-        if( hours || minutes || seconds ){
-          const secondsFromHour = Number.parseInt(hours)*3600
-          const secondsFromMinute = Number.parseInt(minutes)*60
-          const secondsFromSeconds = Number.parseInt(seconds)
-          timeGoal = (
-            (secondsFromHour?secondsFromHour:0) 
-            + (secondsFromMinute?secondsFromMinute:0) 
-            + (secondsFromSeconds?secondsFromSeconds:0)
-          )
-        }
         
         const newActivity = { 
           name, repeatMode, goal, timeGoal: goal=='time'?timeGoal:0, 
@@ -127,10 +116,11 @@ const ActivityFormScreen = ({ navigation, createActivity, updateActivity, goalId
         
         {goal=='time'?
           <TimeInput
-            hours={hours} setHours={setHours} 
-            minutes={minutes} setMinutes={setMinutes} 
-            seconds={seconds} setSeconds={setSeconds}
-            setTimeInputError={setTimeInputError}
+            value={timeGoal}
+            onValueChange={ (value) => {
+              setTimeGoal(value)
+              setTimeInputError(false)
+            }}
           />
         : null }
         {goal=='check' && repeatMode == 'weekly'?
@@ -168,93 +158,6 @@ const ButtonSwitchBar = ({ options, state, setState }) => {
         </Button>
       ))}
     </View>
-  )
-}
-
-const TimeInput = ({ hours, setHours, minutes, setMinutes, seconds, setSeconds, setTimeInputError }) => {
-  const hoursInput = React.useRef()
-  const minutesInput = React.useRef()
-  const secondsInput = React.useRef()
-
-  // used to fix some weird behavior with selection
-  const [hoursSelection, setHoursSelection] = React.useState()
-
-  const [currentFocus, setCurrentFocus] = React.useState()
-
-  const commonProps = {
-    style: {
-      fontSize: 50, padding: 5, textAlign: 'center', 
-      backgroundColor: 'transparent', underlineColorAndroid: 'transparent',
-    },
-    keyboardType: 'number-pad',
-    selectTextOnFocus: true,
-    maxLength: 2,
-    caretHidden: false,
-    underlineColor: 'transparent',
-    selectionColor: 'transparent',
-    //theme: { colors: { primary: 'transparent' } }
-  }
-  const getTheme = (isFocused)=>({ colors: { primary: 'transparent', text: isFocused?'#7B61FF':'black' } })
-
-  return(
-      <View style={{flexDirection: 'row', marginLeft: 20, marginRight: 20, justifyContent: 'center', alignItems: 'center'}}>
-        <TextInput 
-          value={hours} onChangeText={(value) => {
-            value = value<24?value:'24'
-            setHours(value)
-            setHoursSelection({start: value.length, end: value.length})
-            setTimeInputError(false)
-          }} 
-          ref={hoursInput}
-          onFocus={ () => {
-            setHoursSelection({start: 0, end: hours.length})
-            setCurrentFocus('hours')
-          }} 
-          theme={getTheme(currentFocus=='hours')}
-          onBlur={()=>{
-            if(currentFocus=='hours') setCurrentFocus('')
-            setHours(hours.padStart(2, '0'))
-          }}
-          selection={hoursSelection}
-          {...commonProps} 
-        />
-        <Text style={{fontSize: 50, marginBottom: 5}}>:</Text>
-        <TextInput 
-          value={minutes} 
-          onChangeText={(value) => {
-            value = value<59?value:'59'
-            setMinutes(value)
-            setTimeInputError(false)
-          }} 
-          ref={minutesInput} 
-          onFocus={()=>{
-            setCurrentFocus('minutes')
-          }} theme={getTheme(currentFocus=='minutes')}
-          onBlur={()=>{
-            if(currentFocus=='minutes') setCurrentFocus('')
-            setMinutes(minutes.padStart(2, '0'))
-          }}
-          {...commonProps} 
-        />
-        <Text style={{fontSize: 50, marginBottom: 5}}>:</Text>
-        <TextInput 
-          value={seconds} 
-          onChangeText={(value) => {
-            value = value<59?value:'59'
-            setSeconds(value)
-            setTimeInputError(false)
-          }} 
-          ref={secondsInput} 
-          onFocus={()=>{
-            setCurrentFocus('seconds')
-          }} theme={getTheme(currentFocus=='seconds')}
-          onBlur={()=>{
-            if(currentFocus=='seconds') setCurrentFocus('')
-            setSeconds(seconds.padStart(2, '0'))
-          }}
-          {...commonProps} 
-        />
-      </View>
   )
 }
 

commit c4699361a378b90e83882eb29cbcac53b6b7caf7

fix manual time dedication changes
Previously they were not taking any effects.
The bug was introduced in 766bbfb6c88446fcb57829b4d812228273620b10

src/screens/ActivityDetailScreen/TodayPannel.js
@@ -39,28 +39,6 @@ const TodayPannel = ({ entry, toggleCompleted, startTimer, stopTimer, upsertEntr
       upsertEntry({date: date, entry: {...entry, intervals: [newInterval]}})
     }
   
-    function setHours(value){
-      value==''?
-      setTodayTime(todayTime.set({hours: 0})) : value>=23?
-      setTodayTime(todayTime.set({hours: 23})) :
-      setTodayTime(todayTime.set({hours: value}))
-      updateTotalTime(todayTime.as('seconds'))
-    }
-    function setMinutes(value){
-      value==''?
-      setTodayTime(todayTime.set({minutes: 0})) : value>=59?
-      setTodayTime(todayTime.set({minutes: 59})) :
-      setTodayTime(todayTime.set({minutes: value}))
-      updateTotalTime(todayTime.as('seconds'))
-    }
-    function setSeconds(value){
-      value==''?
-      setTodayTime(todayTime.set({seconds: 0})) : value>=59?
-      setTodayTime(todayTime.set({seconds: 59})) :
-      setTodayTime(todayTime.set({seconds: value}))
-      updateTotalTime(todayTime.as('seconds'))
-    }
-  
     const dateIsToday = isToday(date, dayStartHour)
     const activityRunning = isActivityRunning(entry.intervals)
 
@@ -92,6 +70,7 @@ const TodayPannel = ({ entry, toggleCompleted, startTimer, stopTimer, upsertEntr
                   .fromObject({ seconds: value })
                   .shiftTo('hours', 'minutes', 'seconds')
               ) 
+              updateTotalTime(value)
             }} 
             />
           {dateIsToday?