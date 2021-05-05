989b9a0f29acec6b10483b5197c3263445561f38
oliverlsanz <oliverlsanz@gmail.com>
2021-05-05 13:51:16 +0200

    refactor OnboardingScreen

--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,4 +1,4 @@
-import { Image, View, Text, StyleSheet, Linking } from 'react-native';
+import { View, StyleSheet, Linking } from 'react-native';
 import { Title, Paragraph, Subheading } from 'react-native-paper'
 import React from 'react';
 import AppIntroSlider from 'react-native-app-intro-slider';
@@ -6,36 +6,24 @@ import { useTranslation } from 'react-i18next'
 import { OnboardingColor } from '../styles/Colors';
 
 const styles = StyleSheet.create({
-    buttonCircle: {
-      width: 40,
-      height: 40,
-      backgroundColor: OnboardingColor.backgroundColor,
-      borderRadius: 20,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
     slide: {
         alignItems: 'center',
         justifyContent: 'center',
         flex: 1,
+        marginHorizontal: 18,
     }
-    //[...]
   });
 
-
-
- const _renderItem = ({ item }) => {
+ const renderItem = ({ item }) => {
     return(
-        <View style={styles.slide}>
-            <View style={{ alignItems: 'center', justifyContent: 'center', marginHorizontal: 18 }}>
-                <Title style={{ marginBottom: 8}} >{item.title}</Title>
-                <Paragraph style={{ textAlign: 'justify' }} >{item.text}</Paragraph>
-            </View>
-        </View>
+      <View style={styles.slide}>
+        <Title style={{ marginBottom: 8 }} >{item.title}</Title>
+        <Paragraph style={{ textAlign: 'justify' }} >{item.text}</Paragraph>
+      </View>
     )
   }
 
-const NotSoSimple = ({ finishOnboarding }) => {
+const OnboardingScreen = ({ finishOnboarding }) => {
   const { t, i18n } = useTranslation()
 
   const slides = [
@@ -52,25 +40,35 @@ const NotSoSimple = ({ finishOnboarding }) => {
     {
       key: 'three',
       title: t('onboarding.slideThree.title'),
-      text: (
-        <>
-          {t('onboarding.slideThree.text')}
-          <Paragraph style={{color: 'blue'}} onPress={() => Linking.openURL('https://goaliath-app.github.io/guide')}>{t('onboarding.slideThree.linkText')}</Paragraph>
-        </>),
+      text: (<>
+        {t('onboarding.slideThree.text')}
+        <Paragraph 
+          style={{color: 'blue'}} 
+          onPress={() => Linking.openURL('https://goaliath-app.github.io/guide')}>
+            {t('onboarding.slideThree.linkText')}
+        </Paragraph>
+      </>),
     }
   ];
 
   return(
     <AppIntroSlider 
-        renderItem={_renderItem} 
+        renderItem={renderItem} 
         data={slides} 
         onDone={finishOnboarding}
         activeDotStyle={{ backgroundColor: OnboardingColor.activeDot }}
-        renderNextButton={() => <Subheading style={{ marginRight: 12, color: OnboardingColor.buttonTextColor }}>{t('onboarding.next')}</Subheading>}
-        renderDoneButton={() => <Subheading style={{ marginRight: 12, color: OnboardingColor.buttonTextColor }}>{t('onboarding.begin')}</Subheading>}
+        renderNextButton={() => (
+          <Subheading style={{ marginRight: 12, color: OnboardingColor.buttonTextColor }}>
+            {t('onboarding.next')}
+          </Subheading>
+        )}
+        renderDoneButton={() => (
+          <Subheading style={{ marginRight: 12, color: OnboardingColor.buttonTextColor }}>
+            {t('onboarding.begin')}
+          </Subheading>
+        )}
     />
   )
 }
 
-
-export default NotSoSimple;
+export default OnboardingScreen;
