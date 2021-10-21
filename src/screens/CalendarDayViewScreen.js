983c86a30380c5762857683fef37a9c0744fec69
Jimena A <jimenaa971@gmail.com>
2021-10-21 13:19:13 +0200

    add screens for the CalendarScreen
    - add CalendarDayViewScreen
    - add CalendarWeekViewScreen

---
+++ src/screens/CalendarDayViewScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Header } from '../components';
+
+const CalendarDayViewScreen = () => (
+  <View>
+    <Header title={'Day View'} />
+  </View>
+
+)
+
+export default CalendarDayViewScreen;
---
+++ src/screens/CalendarWeekViewScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Header } from '../components';
+
+const CalendarWeekViewScreen = () => (
+  <View>
+    <Header title={'Week View'} />
+  </View>
+)
+
+export default CalendarWeekViewScreen;
