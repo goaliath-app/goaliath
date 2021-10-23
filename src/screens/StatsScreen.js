e8b46a67b9fb23e5bf4d12a04f2a89a316a223b1
OliverLSanz <oliverlsanz@gmail.com>
2021-10-23 13:44:26 +0200

    add charts to StatsScreen

--- src/screens/StatsScreen.js
+++ src/screens/StatsScreen.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { View } from 'react-native'
-import { DayContent } from '../components'
-import { Header } from '../components';
+import { View, ScrollView } from 'react-native'
+import { DayContent, GenericStats, ActivityCalendarHeatmap, Header, ActivityBarChartPicker, StatsPannel } from '../components'
 import { getTodaySelector } from '../redux'
 import { useTranslation } from 'react-i18next'
 import { GeneralColor } from '../styles/Colors';
@@ -11,9 +10,10 @@ const StatsScreen = ({ navigation }) => {
   const { t, i18n } = useTranslation()
 
   return (
-    <View style={{flex: 1, backgroundColor: GeneralColor.screenBackground}}>
+    <ScrollView style={{flex: 1, backgroundColor: GeneralColor.screenBackground}}>
       <Header title={t('statsScreen.headerTitle')} left='hamburger' navigation={navigation} />
-    </View>
+      <StatsPannel />
+    </ScrollView>
   );
 }
 
