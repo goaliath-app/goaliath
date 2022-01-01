9af59315087dde6610984658d4703d4241157068
OliverLSanz <oliverlsanz@gmail.com>
2022-01-01 16:10:52 +0100

    fix OnboardingScreen witdh
    Co-authored-by: Jimena A <jimenaa971@gmail.com>

--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,39 +1,19 @@
-import { View, StyleSheet, Linking } from 'react-native';
-import { Title, Paragraph, Subheading } from 'react-native-paper'
+import { View, Pressable } from 'react-native';
+import { Text } from 'react-native-paper'
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import AppIntroSlider from 'react-native-app-intro-slider';
 import { useTranslation } from 'react-i18next'
-import { OnboardingColor } from '../styles/Colors';
 import { SpeechBubble } from '../components'
 import { setTutorialState } from '../redux'
 import tutorialStates from '../tutorialStates'
-
-const styles = StyleSheet.create({
-    slide: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-        marginHorizontal: 18,
-    }
-  });
-
- const renderItem = ({ item }) => {
-    return(
-      <View style={styles.slide}>
-        <Title style={{ marginBottom: 8 }} >{item.title}</Title>
-        <Paragraph style={{ textAlign: 'justify' }} >{item.text}</Paragraph>
-      </View>
-    )
-  }
 
 const OnboardingScreen = ({ finishOnboarding }) => {
   const { t, i18n } = useTranslation()
   const dispatch = useDispatch()
 
-  const slides = [
-    {
-      text: (<>
+  return (
+    <View style={{flex: 1}}>
+      <View style={{flex: 1, justifyContent: 'center'}}>
         <SpeechBubble speeches={[
           {id: 0, text: t('onboarding.1')},
           {id: 1, text: t('onboarding.2')},
@@ -41,26 +21,14 @@ const OnboardingScreen = ({ finishOnboarding }) => {
           {id: 3, text: ''}
           ]} 
           bubbleStyle={{height: 80}} />
-      </>),
-    }
-  ];
-
-  return(
-    <AppIntroSlider 
-        renderItem={renderItem} 
-        data={slides} 
-        onDone={() => {
-          dispatch(setTutorialState(tutorialStates.Finished))
-          finishOnboarding()
-          }
-        }
-        activeDotStyle={{ backgroundColor: OnboardingColor.activeDot }}
-        renderDoneButton={() => (
-          <Subheading style={{ marginRight: 12, color: OnboardingColor.buttonTextColor }}>
-            {t('onboarding.skip')}
-          </Subheading>
-        )}
-    />
+      </View>
+      <Pressable onPress={() => {
+        dispatch(setTutorialState(tutorialStates.Finished))
+        finishOnboarding()
+      }} style={{alignItems: 'flex-end', marginRight: 25, marginBottom: 20}}>
+        <Text style={{ justifySelf: 'flex-end', fontSize: 16 }}>{t('onboarding.skip')}</Text>
+      </Pressable>
+    </View>
   )
 }
 
