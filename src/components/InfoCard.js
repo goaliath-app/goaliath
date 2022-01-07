c1bc0d6e07e5ce4459e2ddd68a0d8a66a69db7cc
OliverLSanz <oliverlsanz@gmail.com>
2022-01-07 11:04:16 +0100

    add style props to InfoCard

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -3,12 +3,20 @@ import { View } from 'react-native'
 import { Paragraph, Card, Title } from 'react-native-paper'
 import { InfoCardColor } from '../styles/Colors';
 
-const InfoCard = ({title, paragraph, extraContent, style={}}) => (
+const InfoCard = ({
+  title, 
+  paragraph, 
+  extraContent, 
+  style={},
+  cardStyle={},
+  titleStyle={}, 
+  paragraphStyle={}
+}) => (
   <View style={{ margin: 24 }}>
-    <Card style={{backgroundColor: 'aliceblue'}}>
+    <Card style={[{backgroundColor: 'aliceblue'}, cardStyle]}>
       <Card.Content style={style}>
-        { title? <Title>{title}</Title> : null }
-        { paragraph ? <Paragraph>{paragraph}</Paragraph> : null }
+        { title? <Title style={titleStyle}>{title}</Title> : null }
+        { paragraph ? <Paragraph style={paragraphStyle}>{paragraph}</Paragraph> : null }
         {extraContent}
       </Card.Content>
     </Card>
