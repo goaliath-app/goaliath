0870d90adec3f7165d3be1bd7f6e9822c0c23780
Jimena A. <jimenaa971@gmail.com>
2026-06-17 15:19:43 +0200

    chore: reset project

--- src/activityHandler/activityTypes/dailyGoals/index.js
+++
@@ -1,5 +0,0 @@
-import doOneTime from './doOneTime'
-import doNSeconds from './doNSeconds'
-import doNTimes from './doNTimes'
-
-export default { doOneTime, doNSeconds, doNTimes }
--- src/activityHandler/activityTypes/index.js
+++
@@ -1,6 +0,0 @@
-import doFixedDays from './doFixedDays'
-import doNDaysEachWeek from './doNDaysEachWeek'
-import doNSecondsEachWeek from './doNSecondsEachWeek'
-import doNTimesEachWeek from './doNTimesEachWeek'
-
-export default { doFixedDays, doNDaysEachWeek, doNSecondsEachWeek, doNTimesEachWeek }
